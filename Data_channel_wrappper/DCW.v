ebbd596a7967ba00c929e8d8a242a99134793ff5
akhil-123 <1997singhakhil@gmail.com>
2018-06-16 20:55:02 +0530

    data channel wrapper module

--- Data_channel_wrappper/DCW.v
+++ Data_channel_wrappper/DCW.v
@@ -3,28 +3,61 @@ module DCW(
 input [2:0] ctrl_sig ,
 input [2:0] val ,
 input [2:0] val1 ,
-//input [2:0] cl_val ;
+input [24:0] wanted_cl_val ,
+input [24:0] earlier_cl_val ,
 input clock ,
 output reg channel_reset ,
-output reg [2:0] datawidth 
-//output [2:0] ref_clock ;
+output reg [2:0] datawidth, 
+output  reg ref_clock 
 );
+
+reg [24:0] counter ;
+reg temp;
+
+initial begin
+counter = 0 ;
+temp = 0 ;
+end
+
+wire [24:0] count = counter[24] ? (wanted_cl_val) : (wanted_cl_val - earlier_cl_val);
+wire [24:0] final = counter + count;
+
+always @(posedge clock)
+begin
+  if(wanted_cl_val==earlier_cl_val)begin
+  temp=0; end
+else begin
+  counter = final;
+  ref_clock = ~counter[24];
+  temp = 1;
+end
+end
+
+always@(*)
+begin
+if(temp==0)begin
+ref_clock = clock ;end
+end
+//ref_gen_clock c1(wanted_cl_val,earlier_cl_val,clock,ref_clock);
 
 always @(*)begin
 
 if(ctrl_sig==1)begin
 	channel_reset <= 1 ;
 	datawidth <= val ;
+//	ref_gen_clock c1(wanted_cl_val,earlier_cl_val,clock,ref_clock);
+	
 end
 else if(ctrl_sig==2)begin
 	channel_reset <= 0 ;
 	datawidth <=val ;
+//	ref_gen_clock c2(wanted_cl_val,earlier_cl_val,clock,ref_clock);
 end
 else if(ctrl_sig==3)begin
 	channel_reset <= 0 ;
 	datawidth <=val1;
-end
+//	ref_gen_clock c3(wanted_cl_val,earlier_cl_val,clock,ref_clock);
+end	
 
 end
-	
 endmodule
--- Data_channel_wrappper/test_DCW.v
+++ Data_channel_wrappper/test_DCW.v
@@ -3,19 +3,25 @@ module test_DCW;
  reg [2:0] ctrl_sig ;
 reg [2:0] val ;
 reg [2:0] val1 ;
+reg [24:0]wanted_cl_val ;
+reg [24:0]earlier_cl_val ;
 reg clock ;
 
 wire channel_reset ;
 wire [2:0] datawidth ; 
+wire ref_clock ;
  
  // Instantiate the Unit Under Test (UUT)
  DCW uut (
   .ctrl_sig(ctrl_sig), 
   .val(val), 
   .val1(val1),
+  .wanted_cl_val(wanted_cl_val),
+  .earlier_cl_val(earlier_cl_val),
   .clock(clock),
   .channel_reset(channel_reset),
-  .datawidth(datawidth)
+  .datawidth(datawidth),
+  .ref_clock(ref_clock)
  );
   
  initial begin
@@ -28,14 +34,20 @@ wire [2:0] datawidth ;
 	ctrl_sig = 1 ;
 	val = 7 ;
 	val1 = 7 ;
-#70 ;
+	earlier_cl_val = 20000000 ;
+	wanted_cl_val =  20000000 ;
+#400 ;
 	ctrl_sig = 2 ;
 	val = 7 ;
 	val1 = 7 ;
-#140 ;
+	earlier_cl_val = 20000000 ;
+	wanted_cl_val =  10000000 ;
+#800 ;
 	ctrl_sig = 3 ;
 	val = 7 ;
 	val1 = 13 ;
+	earlier_cl_val = 20000000 ;
+	wanted_cl_val =  20000000 ;
  
  end
 
