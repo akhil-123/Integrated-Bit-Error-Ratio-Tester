0ea2bba260b0af7f5c74e0e778e66576605456c1
Akhil Singh <1997singhakhil@gmail.com>
2018-07-31 14:52:54 +0530

    Adding Documentation to Data Channel Wrapper Module

--- IBERT/DCW.v
+++ IBERT/DCW.v
@@ -1,13 +1,18 @@
-
+/* 
+" Data Channel Wrapper Module "
+This module generates the reference clock
+what the user wants from system clock and 
+gives an option to reset the channel.
+*/
 module DCW(
 input [2:0] ctrl_sig ,
 input [2:0] val ,
-input [24:0] wanted_cl_val ,
-input [24:0] earlier_cl_val ,
+	input [24:0] wanted_cl_val , //The clock what the user wants
+	input [24:0] earlier_cl_val , // System clock
 input clock ,
 output reg channel_reset ,
 output reg [2:0] datawidth, 
-output  reg ref_clock 
+output  reg ref_clock // The clock generated from the system clock
 );
 
 reg [24:0] counter ;
@@ -41,11 +46,11 @@ end
 always @(*)begin
 
 if(ctrl_sig==1)begin
-	channel_reset <= 1 ;
+	channel_reset <= 1 ; // 1 means reset the channel
 	datawidth <= val ;
 end
 else if(ctrl_sig==2)begin
-	channel_reset <= 0 ;
+	channel_reset <= 0 ; // 0 means do not reset the channel
 	datawidth <=val ;
 end	
 
