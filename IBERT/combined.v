8735f6cde81d373a36922e7eab3bc26d95d3f295
Akhil Singh <1997singhakhil@gmail.com>
2018-07-31 15:06:30 +0530

    Adding Documentation to Combined Module

--- IBERT/combined.v
+++ IBERT/combined.v
@@ -1,16 +1,21 @@
-
+/*
+" TOP Module "
+This module is the top module.
+It takes parameters which user wants as input
+and gives the BER based on those paramaeters as output
+*/
 module combined(
-input control,
-input sel,
-input [2:0] ctrl_sig,
+input control, // Input for choosing between PRBS-7 or PRBS-13
+input sel, // For choosing between normal input or test pattern input
+input [2:0] ctrl_sig, // This input is for deciding whether to reset the channel or not.  
 input [2:0] val,
-input [24:0] wanted_cl_val ,
-input clock,
-input signed [2:0] taps,
+input [24:0] wanted_cl_val ,// This input refers to clock frequency that the user wants.
+input clock, // The System Clock frequency
+input signed [2:0] taps, // The tap values for pre-emphasis
 input [12:0] normal_input,
 output  [12:0] error,
-output   [12:0] total_error,
-output [31:0] count
+output   [12:0] total_error, // The total number of errors 
+output [31:0] count // The number of bits transmitted 
 );
 wire channel_reset ;
 wire [2:0] datawidth; 
@@ -25,7 +30,7 @@ begin
         frequency = 1.0e9 / (t1 - t0);
 end
 */
-DCW d1(ctrl_sig,val,wanted_cl_val,val1,clock,channel_reset,datawidth,ref_clock);
-test_control test(control,sel,channel_reset,ref_clock,taps,normal_input,error,total_error,count);
+DCW d1(ctrl_sig,val,wanted_cl_val,val1,clock,channel_reset,datawidth,ref_clock);// DCW is the data channel wrapper Module
+test_control test(control,sel,channel_reset,ref_clock,taps,normal_input,error,total_error,count); // The test controller Module
 
 endmodule
