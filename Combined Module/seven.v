655c2761da7cdf686546feaf91867103c72c9899
Akhil Singh <1997singhakhil@gmail.com>
2018-08-09 18:41:09 +0530

    Synthsizable codes

--- Combined Module/seven.v
+++ Combined Module/seven.v
@@ -1,55 +1,22 @@
 /* 	Module for PRBS-7	*/
 /* 
 This module generated PRBS-7 pattern, here the input control is send by user.
-If control =  0, then output of this module will be 0
-and if control = 1, then the output of this module will be PRBS-7
 */
 module seven (
     input control,
     input clock,
     input reset,
-    output [7:0] rnd 
+    output [6:0] rnd 
     );
-reg [7:0] random, random_next, random_done;
-wire feedback = random[6] ^ random[5] ^ random[0]; 
-reg [3:0] count, count_next; //to keep track of the shifts
- 
-always @ (posedge clock or posedge reset)
-begin
- if (reset)
- begin
-  random <= 8'hF; //An LFSR cannot have an all 0 state, thus reset to FF
-  count <= 0;
- end
-  
- else
- begin
-  random <= random_next;
-  count <= count_next;
- end
+    reg [6:0] random;
+wire feedback = random[6] ^ random[0]; 
+always @(posedge clock)begin
+  if (reset==1) begin
+    random <= 7'hf;
+    end
+  else begin
+    random <= {random[6:0], feedback} ;
+    end
 end
- 
-always @ (*)
-begin
-if(control==1)begin
-	random_done = 0;
-end
-else begin
- random_next = random; //default state stays the same
- count_next = count;
-   
-  random_next = {random[6:0], feedback}; //shift left the xor'd every posedge clock
-  count_next = count + 1;
- 
- if (count == 8)
- begin
-  count = 0;
-  random_done = random; //assign the random number to output after 8 shifts
- end  
-end
-end
-assign rnd = random_done;
-//error e1 (random_done,rnd);
- 
+assign rnd = random;
 endmodule
-
--- Combined Module/thirteen.v
+++ Combined Module/thirteen.v
@@ -1,8 +1,6 @@
 /* 	Module for PRBS-13	*/
 /* 
 This module generated PRBS-13 pattern, here the input control is send by user.
-If control =  1, then output of this module will be 0
-and if control = 0, then the output of this module will be PRBS-7
 */
 module thirteen (
     input control,
@@ -10,46 +8,15 @@ module thirteen (
     input reset,
     output [12:0] rnd 
     );
-reg [12:0] random, random_next, random_done;
-wire feedback = random[12] ^ random[3] ^ random[2] ^ random[0]; 
-reg [3:0] count, count_next; //to keep track of the shifts
- 
-always @ (posedge clock or posedge reset)
-begin
- if (reset)
- begin
-  random <= 13'hF; //An LFSR cannot have an all 0 state, thus reset to FF
-  count <= 0;
- end
-  
- else
- begin
-  random <= random_next;
-  count <= count_next;
- end
+    reg [12:0] random;
+wire feedback = random[12] ^ random[3] ^ random[2] ^ random[0];
+always @(posedge clock)begin
+  if (reset==1) begin
+    random <= 13'hf;
+    end
+  else begin
+    random <= {random[12:0], feedback} ;
+    end
 end
- 
-always @ (*)
-begin
-if(control==0)begin
-	random_done = 0;
-end
-else begin
- random_next = random; //default state stays the same
- count_next = count;
-   
-  random_next = {random[11:0], feedback}; //shift left the xor'd every posedge clock
-  count_next = count + 1;
- 
- if (count == 13)
- begin
-  count = 0;
-  random_done = random; //assign the random number to output after 13 shifts
- end
-end  
-end
- 
- 
-assign rnd = random_done;
- 
+assign rnd = random;
 endmodule
