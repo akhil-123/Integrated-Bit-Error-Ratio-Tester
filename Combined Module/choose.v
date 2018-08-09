0542f7dd0c702c44326c1bae96de091dd194498d
Akhil Singh <1997singhakhil@gmail.com>
2018-08-09 16:47:49 +0530

    Corrected number of bits

--- Combined Module/choose.v
+++ Combined Module/choose.v
@@ -1,7 +1,7 @@
 /*
 This Module chooses between PRBS-7 or PRBS-13.
-If the input control is 0, PRBS-13 is selected 
-and if control is 1, PRBS-7 is selected
+If the input control is 0, PRBS-7 is selected 
+and if control is 1, PRBS-13 is selected
 */
 module choose(
 	input control,
@@ -10,16 +10,16 @@ module choose(
 	output reg [12:0] pattern
 //	output [12:0] error
 );
-wire [7:0]rnd;
+wire [6:0]rnd;
 wire [12:0]rnd1 ;
 seven s1(control,clock,reset,rnd);
 thirteen tt1(control,clock,reset,rnd1);
 always @(*)begin
-if(rnd==0)begin
-pattern <=rnd1 ;
-end
-else if(rnd1==0)begin
+if(control==0)begin
 pattern <=rnd ;
+end
+else if(control==1)begin
+pattern <=rnd1 ;
 end
 end
 //error_adder e1(pattern,error);
